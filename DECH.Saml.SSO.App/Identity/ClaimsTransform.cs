e2769e833c6ae0de7b794ed0dced94468d79a3b7
huangal <henry@huangal.com>
2020-11-24 22:15:58 -0600

    Initial check-in of module DECH.Saml.SSO.App

---
+++ DECH.Saml.SSO.App/Identity/ClaimsTransform.cs
@@ -0,0 +1,58 @@
+﻿using ITfoxtec.Identity.Saml2.Claims;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+namespace DECH.Saml.SSO.App.Identity
+{
+    public static class ClaimsTransform
+    {
+        public static ClaimsPrincipal Transform(ClaimsPrincipal incomingPrincipal)
+        {
+            if (!incomingPrincipal.Identity.IsAuthenticated)
+            {
+                return incomingPrincipal;
+            }
+
+            //var claims = GetSaml2LogoutClaims(incomingPrincipal).ToList();
+
+
+            return CreateClaimsPrincipal(incomingPrincipal);
+        }
+
+        private static ClaimsPrincipal CreateClaimsPrincipal(ClaimsPrincipal incomingPrincipal)
+        {
+            var claims = new List<Claim>();
+
+            // All claims
+            claims.AddRange(incomingPrincipal.Claims);
+
+            // Or custom claims
+            //claims.AddRange(GetSaml2LogoutClaims(incomingPrincipal));
+            //claims.Add(new Claim(ClaimTypes.NameIdentifier, GetClaimValue(incomingPrincipal, ClaimTypes.NameIdentifier)));
+
+            return new ClaimsPrincipal(new ClaimsIdentity(claims, incomingPrincipal.Identity.AuthenticationType, ClaimTypes.NameIdentifier, ClaimTypes.Role)
+            {
+                BootstrapContext = ((ClaimsIdentity)incomingPrincipal.Identity).BootstrapContext
+            });
+        }
+
+        private static IEnumerable<Claim> GetSaml2LogoutClaims(ClaimsPrincipal principal)
+        {
+            yield return GetClaim(principal, Saml2ClaimTypes.NameId);
+            yield return GetClaim(principal, Saml2ClaimTypes.NameIdFormat);
+            yield return GetClaim(principal, Saml2ClaimTypes.SessionIndex);
+        }
+
+        private static Claim GetClaim(ClaimsPrincipal principal, string claimType)
+        {
+            return ((ClaimsIdentity)principal.Identity).Claims.Where(c => c.Type == claimType).FirstOrDefault();
+        }
+
+        private static string GetClaimValue(ClaimsPrincipal principal, string claimType)
+        {
+            var claim = GetClaim(principal, claimType);
+            return claim != null ? claim.Value : null;
+        }
+    }
+}
---
+++ DECH.Saml.SSO.App/Pages/ClaimsModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace DECH.Saml.SSO.App.Pages
+{
+    [Authorize]
+    public class ClaimsModel : PageModel
+    {
+        private readonly ILogger<ClaimsModel> _logger;
+
+        public ClaimsModel(ILogger<ClaimsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
