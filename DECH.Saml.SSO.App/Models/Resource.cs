388a9d977d55cc3fb9cab9e2476d1259fa10f89c
huangal <henry@huangal.com>
2020-11-26 11:38:56 -0600

    Add form to access controll on index

---
+++ DECH.Saml.SSO.App/Models/Resource.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace DECH.Saml.SSO.App.Models
+{
+    public class Resource
+    {
+        public string ResourceUrl { get; set; } = "https://localhost:5002/Privacy";
+    }
+}
